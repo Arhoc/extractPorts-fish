8a513367e2d7ba8f6178324081be7e69de69adff
Aidon <la_archi@protonmail.com>
2025-03-06 18:43:30 -0600

    Update extractPorts.fish

--- extractPorts.fish
+++ extractPorts.fish
@@ -1,7 +1,17 @@
 function extractPorts
-    set ports (cat argv[1] | grep -oP '\d{1,5}/open' | awk '{print 1}')
-    set ip_address (cat argv[1]∣grep−oP′\d1,3\d˙​1,3\d˙​1,3\d˙​1,3′)echo−e"\n[∗]Extractinginformation...\n">extractPorts.tmpecho−e"\t[∗]IPAddress:ip_address"  >> extractPorts.tmp
-    echo -e "\t[] Open ports: ports\n">>extractPorts.tmpechoports | tr -d '\n' | xclip -sel clip
-    echo -e "[] Ports copied to clipboard\n"  >> extractPorts.tmp
+    set input_file $argv[1]
+    if test -z "$input_file"
+        set input_file /dev/stdin
+    end
+
+    set ports (grep -oP '\d+/tcp open' $input_file | awk '{print $1}' | sed -e 's/\/tcp//' | tr '\n' ',' | sed 's/,$//')
+    set ip_address (grep -oP 'Scan.+?\K\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' $input_file | head -n 1)
+
+    echo -e "\n[*] Extracting information...\n" > extractPorts.tmp
+    echo -e "\t[*] IP Address: $ip_address" >> extractPorts.tmp
+    echo -e "\t[*] Open ports: $ports\n" >> extractPorts.tmp
+    
+    echo $ports | tr -d '\n' | xclip -sel clip
+    echo -e "[*] Ports copied to clipboard\n" >> extractPorts.tmp
     cat extractPorts.tmp; rm extractPorts.tmp
 end
